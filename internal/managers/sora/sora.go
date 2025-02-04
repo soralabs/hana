aec951b9d77b42b91b2407ee90763ed08ba1f0bd
just-stv <22718607+just-stv@users.noreply.github.com>
2025-02-03 22:51:05 -0800

    feat: add dexscreener information

---
+++ internal/managers/sora/sora.go
@@ -0,0 +1,1 @@
+package sora_manager
---
+++ internal/managers/sora/types.go
@@ -0,0 +1,12 @@
+package sora_manager
+
+import (
+	"github.com/soralabs/zen/manager"
+	"github.com/soralabs/zen/options"
+)
+
+// SoraManager handles Sora-specific behavior and responses, such as information about Sora
+type SoraManager struct {
+	*manager.BaseManager
+	options.RequiredFields
+}
